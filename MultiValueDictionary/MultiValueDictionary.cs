b2bca31c57f89e93d78144d2940084111d5ab02a
Samvel Yeghikyan <Samvel.Yeghikyan@technamin.com>
2023-11-13 22:22:08 +0400

    Add project files.

---
+++ MultiValueDictionary/MultiValueDictionary.cs
@@ -0,0 +1,72 @@
+﻿namespace MultiValueDictionary
+{
+    class MultiValueDictionary<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, List<TValue>> _dic;
+
+        public MultiValueDictionary()
+        {
+            _dic = new Dictionary<TKey, List<TValue>>();
+        }
+
+        public IEnumerable<TKey> Keys => _dic.Keys;
+        public int Count => _dic.Count;
+        public void Add(TKey key, TValue value)
+        {
+            if (_dic.ContainsKey(key))
+            {
+                _dic[key].Add(value);
+            }
+            else
+            {
+                _dic.Add(key, new List<TValue>() { value });
+            }
+        }
+        public void Clear()
+        {
+            _dic.Clear();
+        }
+        public bool ContainsKey(TKey key)
+        {
+            return _dic.ContainsKey(key);
+        }
+        public void Remove(TKey key)
+        {
+            _dic.Remove(key);
+        }
+        public void Remove(TKey key, TValue value)
+        {
+            if (_dic.TryGetValue(key, out var list))
+            {
+                list.Remove(value);
+            } else
+            throw new KeyNotFoundException();
+        }
+        public List<TValue> this[TKey key]
+        {
+            get
+            {
+                if (_dic.TryGetValue(key, out var list))
+                    return list;
+                throw new KeyNotFoundException();
+            }
+        }
+        public List<TValue> Get(TKey key)
+        {
+            if (_dic.TryGetValue(key, out var list))
+                return list;
+            throw new KeyNotFoundException();
+        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach (var item in _dic)
+            {
+                var values = item.Value;
+                foreach (var value in values)
+                {
+                    yield return new KeyValuePair<TKey, TValue>(item.Key, value);
+                }
+            }
+        }
+    }
+}
---
+++ MultiValueDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿class MyClass
+{
+    public static void Main()
+    {
+        var dic = new MultiValueDictionary.MultiValueDictionary<int, string>();
+
+        dic.Add(1, "dasd");
+        dic.Add(1, "iopui");
+        dic.Add(2, "dasdfgd");
+
+        dic.Add(3, "dfgdasd");
+        dic.Add(4, "dgdasdgdd");
+
+        dic.Add(12, "dfgQWEREdasd");
+        dic.Add(15, "dasd");
+
+        dic.Add(12, "dasd");
+
+
+
+        foreach (var item in dic)
+        {
+            Console.WriteLine(item.Key + " // " + item.Value);
+        }
+
+        var kk = dic.Get(1);
+        dic.Remove(1, "dasd");
+        var ddd = dic[1];
+        dic.Clear();
+    }
+
+}
